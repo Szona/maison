d4a6e783f8681895e7bc27d27e2207e3267d447e
Szona <cmielalicja@gmail.com>
2018-03-12 13:12:24 +0100

    scss base

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,23 @@
 var gulp = require("gulp");
 var sass = require('gulp-sass');
-//var jshint = require('gulp-jshint'); 
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
 var minify = require('gulp-minify');
-
-
-
-
-
 
 gulp.task('sass', function() {
 
     return gulp.src('scss/style.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({
-        outputStyle: 'expanded', 
+        outputStyle: 'expanded',
         errLogToConsole: true}))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('css'));
 
 });
 
-gulp.task('watch', ['sass'], function(){ //zadanie [sass] wykonanie przed watch, po wywołaniu watch
+gulp.task('watch', ['sass'], function(){
 
     gulp.watch('scss/**/*.scss', ['sass']);
-    
+
 });
-
-
