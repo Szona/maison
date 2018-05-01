d18110a4d4d13a9998f9c96134a839b7e9c1dd83
Szona <cmielalicja@gmail.com>
2018-05-01 11:41:04 +0200

    hamburger menu

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+//navigation
   const menuWraper = document.getElementsByClassName("menu-wrapper");
   const hamburger = document.querySelector(".hamburger-menu");
 
-  // console.log(hamburger);
   function toggleMenu() {
     hamburger.classList.toggle("animate");
   };
+
   menuWraper[0].addEventListener("click", toggleMenu);
 
 
