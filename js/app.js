19e8323afcf1ea37019cdfd1ddee0ac734344b0c
Szona <cmielalicja@gmail.com>
2018-05-01 11:43:44 +0200

    setInterval on dots/slider

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+
+
 //navigation
   const menuWraper = document.getElementsByClassName("menu-wrapper");
   const hamburger = document.querySelector(".hamburger-menu");
@@ -12,3 +14,32 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 
+//slider
+  const sliderCircles = Array.prototype.slice.call( document.querySelectorAll(".circle"));
+  const circlesLen = sliderCircles.length;
+  let activeCircleIndex = 0;
+
+  //find index of active circle with .visible class in sliderCircles array and and move it to the next element;
+  function switchActiveCircle() {
+    let activeCircle = document.querySelector(".circle.visible");
+
+    if( sliderCircles.indexOf(activeCircle) == circlesLen-1 ) {
+      activeCircleIndex = 0;
+    } else {
+      activeCircleIndex++;
+    }
+  };
+  //find active circle and remove form it the .visible class and give it to the next one
+  function toggleActiveCircle() {
+    let activeCircle = document.querySelector(".circle.visible");
+
+    switchActiveCircle();
+
+    activeCircle.classList.remove("visible");
+    document.querySelectorAll(".circle")[activeCircleIndex].classList.add("visible");
+  };
+
+  setInterval( toggleActiveCircle, 2000 );
+
+
+}, false);
